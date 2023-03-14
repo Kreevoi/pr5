de36a040e3a25e31bfcf47fd7b336007ed612914
Kreevoi <92435504+Kreevoi@users.noreply.github.com>
2023-03-14 11:41:21 +0700

    Add files via upload

---
+++ GitResosNekrasov/app/src/main/java/com/example/gitresosnekrasov/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.gitresosnekrasov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override//да
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
